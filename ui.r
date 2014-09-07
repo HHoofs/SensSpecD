797869e33886b5c11e39a51d36e9f60dde8d38f5
schepijs <huubhoofs@gmail.com>
2014-09-07 21:53:05 +0200

    Uitleg

--- ui.r
+++ ui.r
@@ -52,7 +52,11 @@ shinyUI(
                                      # Opties voor tabel
                                      wellPanel(strong("Tabel"), 
                                                checkboxInput("TA_inv","Invullen",value=FALSE),
-                                               checkboxInput("TA_uit","Uitrekenen"),value=FALSE)
+                                               checkboxInput("TA_uit","Uitrekenen"),value=FALSE),
+                                     checkboxInput("expl",strong("Uitleg")),
+                                     conditionalPanel(condition = paste("input.expl == true"),
+                                                      "Voor een bepaalde ziekte zijn 21 personen gescreend met een nieuw instrument. De scores lopen van 0 t/m 105, waarbij een hogere score staat voor een grotere kans op de ziekte. In werkelijkheid hebben de groene personen de ziekte niet, en de rode personen wel. Het afkappunt kan (boven de figuur) worden verschoven om zodoende te kijken welke gevolgen dit heeft voor de sensitiviteit, specificiteit, NPV, en PPV. De optie Cell onder Figuur geeft de locatie van ieder persoon in de 2x2 tabel aan. De optie Classificatie toont aan of met het desbetreffende afkappunt een persoon juist of onjuist is geclassificeerd (X = Fout; V = Goed). De opties Invullen en Uitrekenen onder Tabel, tonen, respectievelijk, de aantallen in de 2x2 tabel en de bijbehorende sensitiviteit, specificiteit, NPV, en PPV waardes. De figuur rechtsonder geeft de ROC curve weer, waarbij de sensitiviteit en specificiteit van het gekozen afkappunt zijn omcirkeld. Bij condities kunnen verschillende situaties worden gekozen m.b.t. de prior kans van de ziekte en de kwaliteit van het instrument.")
+                                     
                                    ),
                                    
                                    mainPanel(
